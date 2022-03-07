adec15026029f250bc7e1bf796f6762787571386
David Brown <dbrown92700@gmail.com>
2022-03-07 16:28:15 -0500

    auto ip

--- atticfan.py
+++ atticfan.py
@@ -3,6 +3,7 @@
 from flask import Flask, request, make_response, render_template, redirect
 from threading import Thread
 from time import sleep
+import socket
 
 ###########################################################################
 # Set up global variables for time remaining and a countdown thread object
@@ -59,5 +60,6 @@ if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    ipaddress = socket.gethostbyname(socket.gethostname())
+    app.run(host=ipaddress, port=8080, debug=True)
 # [END gae_python38_app]

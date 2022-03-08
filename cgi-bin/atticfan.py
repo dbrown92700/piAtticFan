4fca216b2320279d93f91bcbb09e4a22c3fbd677
David Brown <dbrown92700@gmail.com>
2022-03-08 17:21:13 -0500

    Finished basic timer

--- cgi-bin/atticfan.py
+++ cgi-bin/atticfan.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import os
 
-from flask import Flask, request, make_response, render_template, redirect
+from flask import Flask, request, make_response, render_template, redirect, jsonify
 from threading import Thread
 from time import sleep
 import pigpio
@@ -9,7 +9,8 @@ import pigpio
 ###########################################################################
 # Set up global variables for time remaining and a countdown thread object
 ###########################################################################
-time_remaining = 60
+time_remaining = 0
+status = 'Is Not Running'
 pi = pigpio.pi()
 highpin = 17
 pi.set_mode(highpin, pigpio.OUTPUT)
@@ -18,21 +19,39 @@ pi.set_pull_up_down(highpin, pigpio.PUD_DOWN)
 def countdown():
     global time_remaining
     global pi
+    global status
 
     while time_remaining > 0:
         sleep(1)
         time_remaining -= 1
     print('Done')
+    time_remaining = 0
+    status = 'Is Not Running'
     pi.write(highpin, 0)
 
 timer = Thread(target=countdown)
 
 app = Flask(__name__)
 app.secret_key = 'any random string'
+###########################################################################
+# read page reads the time remaining or indicates if the timer thread is
+# not running
+###########################################################################
 
 @app.route('/')
-def home():
-    return app.root_path
+def controller():
+
+    global timer
+    global status
+
+    if timer.is_alive():
+        status = 'Is Running'
+        time = f'{int(time_remaining/1800):2}:{int((time_remaining%1800)/60):02}:{time_remaining%60:02}'
+    else:
+        status = 'Is Not Running'
+        time = 'N/A'
+
+    return make_response(render_template('fan.html', time=time, status=status))
 
 
 ###########################################################################
@@ -52,30 +71,48 @@ def set():
         timer.start()
     pi.write(highpin, 1)
 
-    return make_response(redirect('/read/'))
+    return make_response(redirect('/'))
 
 ###########################################################################
-# read page reads the time remaining or indicates if the timer thread is
-# not running
+# start page sets the time remaining value and starts the timer thread
+# if it's not running already
 ###########################################################################
+@app.route('/stop/')
+def stop():
 
-@app.route('/read/')
-def read():
-
+    global time_remaining
     global timer
-    if timer.is_alive():
-        status = 'Is Running'
-        time = time_remaining
-    else:
-        status = 'Is Not Running'
-        time = 'N/A'
+    global pi
 
-    return make_response(render_template('fan.html', time=time, status=status))
+    time_remaining = 0
+    pi.write(highpin, 0)
+
+    return make_response(redirect('/'))
+
+###########################################################################
+# Web call that returns the amount of time on the clock
+###########################################################################
+
+@app.route('/clock/')
+def clock():
+
+    global time_remaining
+
+    clock = f'{int(time_remaining/1800):2}:{int((time_remaining%1800)/60):02}:{time_remaining%60:02}'
+    return clock
+
+@app.route('/status/')
+def fanstat():
+
+    global status
+
+    return status
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
+    # Set PI_IP_ADD environment variable with the server IP address
     ipaddress = os.getenv('PI_IP_ADD', '127.0.0.1')
     app.run(host=ipaddress, port=8080, debug=True)
 # [END gae_python38_app]
